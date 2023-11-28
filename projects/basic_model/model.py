ba0c3550a10c34fac70dde5f5de2325f7dbffde9
ilovemyminutes <silkstaff@naver.com>
2023-11-28 20:40:32 +0900

    reviewed by ilovemyminutes

--- projects/basic_model/model.py
+++ projects/basic_model/model.py
@@ -1,18 +1,19 @@
 import torch
 import torch.nn.functional as F
 
+
 class CustomModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.Lin1 = torch.nn.Linear(4, 16)
-        self.Lin2 = torch.nn.Linear(16, 3)
+        self.linear0 = torch.nn.Linear(4, 16)
+        self.linear1 = torch.nn.Linear(16, 3)
 
     def forward(self, x):
         x = F.relu(self.Lin1(x))
         return F.softmax(self.Lin2(x), dim=1)
-    
 
-if __name__ == '__main__':
-    CM = CustomModel()
-    print(f"CM: {CM}")
-    print(CM.Lin1.weight.dtype)
+
+if __name__ == "__main__":
+    model = CustomModel()
+    print(f"CustomModel: {model}")
+    print(model.linear1.weight.dtype)
--- projects/basic_model/train.py
+++ projects/basic_model/train.py
@@ -1,76 +1,103 @@
 import torch
-import model
-import dataloader
+from torch import nn, optim
+from torch.optim import Optimizer
+from model import CustomModel
+from torch.utils.data import DataLoader
+from dataset import IrisDataset
 import matplotlib.pyplot as plt
 
-def main():
-    IrisTrainDataLoader = dataloader.IrisDataLoader(is_train=True).IrisDataLoader
-    IrisTestDataLoader = dataloader.IrisDataLoader(is_train=False).IrisDataLoader
 
-    Model = model.CustomModel()
-    optm = torch.optim.Adam(Model.parameters())
-    loss_fn = torch.nn.CrossEntropyLoss()
+def train_one_epoch(model: nn.Module, dataloader: DataLoader, optimizer: Optimizer, loss_fn: nn.Module):
+    loss_sum, acc_sum = 0, 0
+    for x, y_true in dataloader:
+        y_pred = model(x)
+
+        optimizer.zero_grad()
+        loss = loss_fn(y_pred, y_true)
+        loss.backward()
+        optimizer.step()
+
+        loss_sum += loss.item()
+        acc_sum += int(sum(y_true.argmax(dim=1) == y_pred.argmax(dim=1)))
+
+    loss = loss_sum / (120 / 8)
+    acc = acc_sum / 120
+    return loss, acc
+
+
+def test_one_epoch(model: nn.Module, dataloader: DataLoader, loss_fn: nn.Module):
+    loss_sum, acc_sum = 0, 0
+    with torch.no_grad():
+        for x, y_true in dataloader:
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y_true)
+            loss_sum += loss.item()
+            acc_sum += int(sum(y.argmax(dim=1) == y_.argmax(dim=1)))
+    loss = loss_sum / (30 / 8)
+    acc = acc_sum / 30
+    return loss, acc
+
+
+def main():
+    train_dataset = IrisDataset()
+    test_dataset = IrisDataset(is_train=False)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=8)
+    test_dataloader = DataLoader(test_dataset, batch_size=8)
+
+    model = CustomModel()
+    optimizer = optim.Adam(model.parameters())
+    loss_fn = nn.CrossEntropyLoss()
 
     train_loss, train_acc, test_loss, test_acc = [], [], [], []
 
     for epoch in range(50):
-        # Train
-        loss_sum, acc_sum = 0, 0
-        for x, y in IrisTrainDataLoader:
-            y_ = Model(x)
-
-            optm.zero_grad()
-            loss = loss_fn(y_, y)
-            loss.backward()
-            optm.step()
-
-            loss_sum += loss.item()
-            acc_sum += int(sum(y.argmax(dim=1) == y_.argmax(dim=1)))
-
-        train_loss.append(loss_sum/(120/8))
-        train_acc.append(acc_sum/120)
-
-        # Test
-        loss_sum, acc_sum = 0,0
-        with torch.no_grad():
-            for x, y in IrisTestDataLoader:
-                y_ = Model(x)
-
-                loss = loss_fn(y_, y)
-
-                loss_sum += loss.item()
-                acc_sum += int(sum(y.argmax(dim=1) == y_.argmax(dim=1)))
-
-        test_loss.append(loss_sum/(30//8))
-        test_acc.append(acc_sum/30)
-
+        train_epoch_loss, train_epoch_acc = train_one_epoch(model, train_dataloader, optimizer, loss_fn)
+        test_epoch_loss, test_epoch_acc = test_one_epoch(model, test_dataloader, loss_fn)
         # Print Loss and Accuracy
-        if epoch%10 == 0:
-            print(f"[Epochs]:{epoch}\n[Train Loss]:{train_loss[-1]}, [Train Accuracy]:{train_acc[-1]}\n[Test Loss]:{test_loss[-1]}, [Test Accuracy]:{test_acc[-1]}")
+        if epoch % 10 == 0:
+            print(
+                f"[Epochs]:{epoch}\n"
+                f"[Train Loss]:{train_epoch_loss}, "
+                f"[Train Accuracy]:{train_epoch_acc}\n"
+                f"[Test Loss]:{test_epoch_loss}, "
+                f"[Test Accuracy]:{test_epoch_acc}"
+            )
+        train_loss.append(train_epoch_loss)
+        train_acc.append(train_epoch_acc)
+        test_loss.append(test_epoch_loss)
+        test_acc.append(test_epoch_acc)
 
     # Print Label and Prediction
     with torch.no_grad():
-        x, y = next(iter(IrisTestDataLoader))
-        y_ = Model(x)
+        x, y_true = next(iter(test_dataloader))
+        y_pred = model(x)
 
-        y = [['Iris-setosa','Iris-versicolor','Iris-virginica'][idx] for idx in y.argmax(dim=1)]
-        y_ = [['Iris-setosa','Iris-versicolor','Iris-virginica'][idx] for idx in y_.argmax(dim=1)]
+        y_true = [
+            ["Iris-setosa", "Iris-versicolor", "Iris-virginica"][idx]
+            for idx in y.argmax(dim=1)
+        ]
+        y_pred = [
+            ["Iris-setosa", "Iris-versicolor", "Iris-virginica"][idx]
+            for idx in y_.argmax(dim=1)
+        ]
 
-        print(y)
-        print(y_)
+        print(y_true)
+        print(y_pred)
 
     # Save Fig.
-    fig, axes = plt.subplots(1,2)
-    for i, value in enumerate(['Loss', 'Accuracy']):
+    fig, axes = plt.subplots(1, 2)
+    for i, value in enumerate(["Loss", "Accuracy"]):
         axes[i].set_title(value)
-        axes[i].set_xlabel('Epochs')
-        if value == 'Loss':
-            axes[i].plot(train_loss, label = 'Train')
-            axes[i].plot(test_loss, label = 'Test')
+        axes[i].set_xlabel("Epochs")
+        if value == "Loss":
+            axes[i].plot(train_loss, label="Train")
+            axes[i].plot(test_loss, label="Test")
         else:
-            axes[i].plot(train_acc, label = 'Train')
-            axes[i].plot(test_acc, label = 'Test')
+            axes[i].plot(train_acc, label="Train")
+            axes[i].plot(test_acc, label="Test")
         axes[i].legend()
-    fig.savefig('result')
+    fig.savefig("result")
+
 
 main()
